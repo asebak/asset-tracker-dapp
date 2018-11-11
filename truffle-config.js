e011d4e00a3854171466c12f5592f4c8287bdcac
asebak <radio_frequency@hotmail.com>
2018-11-11 06:17:23 -0500

    update build script

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  migrations_directory: "./migrations",
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
+    migrations_directory: "./migrations",
+    networks: {
+        development: {
+            host: "localhost",
+            port: 9545,
+            network_id: "*" // Match any network id
+        }
+    },
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 500
+        }
     }
-  }
 };
