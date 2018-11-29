c557890da51793320270df2086f6d61b7dd634d6
Ahmad <radio_frequency@hotmail.com>
2018-11-29 04:27:26 -0500

    rename ui elements

--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -4,13 +4,24 @@ import { ContractData, ContractForm } from 'drizzle-react-components'
 class Home extends Component {
   render() {
     return (
-          <div className="pure-u-1-1">
+          /*<div className="pure-u-1-1">
             <h2>Calorie Tracker</h2>
             <p>This shows a simple ContractData component with no arguments, along with a form to set its value.</p>
             <p><strong>Stored Value</strong>: <ContractData contract="CalorieTracker" method="settings"  methodArgs={[this.props.accounts[0]]} /></p>
             <ContractForm contract="CalorieTracker" method="addSettings" />
             <br/><br/>
           </div>
+          */
+        <div className="pure-u-1-1">
+            <h2>Asset Tracker</h2>
+            <form className="pure-form">
+                <fieldset>
+                    <legend>View an asset's history</legend>
+                    <input type="text" placeholder="Id"/>
+                    <button type="submit" className="pure-button pure-button-primary">View History</button>
+                </fieldset>
+            </form>
+        </div>
     )
   }
 }
--- src/layouts/registerasset/RegisterAsset.js
+++ src/layouts/registerasset/RegisterAsset.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 
-class Journal extends Component {
+class RegisterAsset extends Component {
   render() {
     return (
           <div className="pure-u-1-1">
-            <h2>Journal</h2>
+            <h2>Register Asset</h2>
             <br/><br/>
           </div>
     )
   }
 }
 
-export default Journal
+export default RegisterAsset
