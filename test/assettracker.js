ed3ba514bd756e76f8708e21c38401383c3e3f85
Ahmad <radio_frequency@hotmail.com>
2018-12-22 20:27:57 -0500

    update test cases documentation

--- test/assettracker.js
+++ test/assettracker.js
@@ -8,14 +8,16 @@ contract('AssetTracker', function(accounts) {
         const name = "IoTGateway";
         const time = 1;
         const id = "0x4d20000000000000000000000000000000000000000000000000000000000000";
+        //creating an asset
         const result = await instance.registerAsset(time, name, id);
+        //asserting that the event is called
         assert.web3Event(result, {
             event: 'AssetCreated',
             args: {
                 id: id, //32 bytes length
             }
         }, 'The event is emitted');
-
+        //fetching the asset to verify it was created.
         const asset = await instance.fetchAsset(id);
         assert.strictEqual(asset[0], name, 'asset name should be set');
     });
@@ -44,8 +46,10 @@ contract('AssetTracker', function(accounts) {
         const time = 1;
         const id = "0x4d73670000000000000000000000000000000000000000000000000000000000";
         await instance.registerAsset(time, name, id, {from: accounts[0]});
+        //after registering an asset a default create event is added to the asset
         const assetEvent = await instance.fetchEvent.call(id);
         const intialEvent = "created";
+        //verifying that the event was created with this asset.
         assert.strictEqual(assetEvent[0].toLowerCase(), intialEvent, 'event name should be set');
         assert.strictEqual(assetEvent[1].valueOf(), '0', 'event type should be set');
     });
@@ -67,7 +71,9 @@ contract('AssetTracker', function(accounts) {
         const eventType = "1"; //location
         const data = ["testing data"];
         await instance.registerAsset(time, name, id, {from: accounts[0]});
+        //adding a new event to the asset
         const result = await instance.addEvent(id, eventId,  eventName, eventType, data.map((arg) => web3.toHex(arg)), time, {from: accounts[0]});
+        //verifying an event was triggered for adding a new asset event.
         assert.web3Event(result, {
             event: 'AssetEventCreated',
             args: {
@@ -84,6 +90,7 @@ contract('AssetTracker', function(accounts) {
 
     it("...can pause a contract as an admin", async () => {
         const instance = await AssetTracker.new();
+        //pausing the contract
         await instance.pause();
         var isPaused = await instance.paused();
         assert.strictEqual(isPaused, true, 'contract is paused');
@@ -91,10 +98,13 @@ contract('AssetTracker', function(accounts) {
     });
 
     it("...can destroy a contract as the admin", async () => {
+        //when we destroy the contract that code is 0x0 bytes when we get the code.
         const instance = await AssetTracker.new();
         await instance.pause();
+        //get original code from the contract
         var result = web3.eth.getCode(instance.address);
         await instance.destroyContract();
+        //get code after contract is destroyed.
         var result2 = web3.eth.getCode(instance.address);
         assert.notStrictEqual(result, result2, 'contract was destroyed');
     });
